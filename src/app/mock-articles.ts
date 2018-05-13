f7c9495860890c3ff2e0e0cf297e272776c9335b
joe mckinney <mckinney.a.joe@gmail.com>
2018-05-13 13:12:10 -0700

    move the hard coded articles to a mock-articles file from the home component

---
+++ src/app/mock-articles.ts
@@ -0,0 +1,10 @@
+import { Article } from './article.model';
+
+export const ARTICLES: Article[] = [
+  new Article("Space", "Space is super cool.", "Joseph McKinney", "Here is an example article body about space.", "https://img.purch.com/w/660/aHR0cDovL3d3dy5zcGFjZS5jb20vaW1hZ2VzL2kvMDAwLzA2MS8wNzUvb3JpZ2luYWwvY2hhbmRyYS1uZ2M2MzU3LWNvbXBvc2l0ZS5qcGc=", 1),
+  new Article("The Beach is Fun", "If you haven't been to the beach lately...", "Joe McKinney", "Here is another example article body.  This time it is about the beach.", "https://images.pexels.com/photos/189349/pexels-photo-189349.jpeg?auto=compress&cs=tinysrgb&h=350", 2),
+  new Article("The Mountains", "The mountains are really fun.", "Joey McKinney", "Here is an example article body this time about the mountains.", "https://images.unsplash.com/photo-1518021964703-4b2030f03085?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=67eedb090e43beecf456d519c3fca86c&w=1000&q=80", 3),
+  new Article("Lakes are Amazing", "Lucky to be able to go to lakes", "Joe A. McKinney", "Here is another example article body.  This time it is about lakes.", "https://upload.wikimedia.org/wikipedia/commons/2/23/Lake_mapourika_NZ.jpeg", 4),
+  new Article("Jungles are rad", "Radness to be found everywhere in the joungles.", "Joseph A. McKinney", "Here is an example article body about Jungles.", "https://www.nature.org/cs/groups/webcontent/@web/@brasil/documents/media/forest-in-the-pot-kro-villag-1.jpg", 5),
+  new Article("The Desert", "If you haven't been to the desert...", "J.A. McKinney", "Here is another example article body.  This time it is about deserts.", "https://res.cloudinary.com/dk-find-out/image/upload/q_80,w_1920,f_auto/Sand_Dunes_23_4_13_qn3vac.jpg", 6),
+];
