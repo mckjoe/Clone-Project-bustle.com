8f2c4ad883cef3a36e44a1106c8887c3f91c78ca
joe mckinney <mckinney.a.joe@gmail.com>
2018-05-13 16:24:32 -0700

    add media query for responsiveness for mobile phones

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,6 +13,7 @@ import { FirebaseListObservable } from 'angularfire2/database';
 export class HomeComponent implements OnInit {
 
   articles: FirebaseListObservable<any[]>;
+  currentRoute: string = this.router.url;
 
   constructor(private router: Router, private articleService: ArticleService) { }
 
