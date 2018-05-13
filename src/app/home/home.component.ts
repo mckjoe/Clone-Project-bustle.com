5b0fca34aa5ec2a09f0d529db0b8eb648dbb57f4
joe mckinney <mckinney.a.joe@gmail.com>
2018-05-13 13:24:47 -0700

    update project to use article service to provide articles to be displayed

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,22 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Article } from '../article.model';
+import { ArticleService } from '../article.service';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [ArticleService]
 })
 export class HomeComponent implements OnInit {
 
   articles: Article[];
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private articleService: ArticleService) { }
 
   goToReadPage(clickedArticle: Article) {
     this.router.navigate(['articles', clickedArticle.id]);
   }
 
   ngOnInit() {
+    this.articles = this.articleService.getArticles();
   }
 
 }
